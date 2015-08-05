d5797540b77f60238758f8f304202196cf1652f5
Joerg Domaschka <joerg.domaschka@uni-ulm.de>
2015-08-05 13:51:24 +0200

    simplify comparsion.

--- src/main/java/de/uniulm/omi/cloudiator/lance/lca/container/port/DownstreamAddress.java
+++ src/main/java/de/uniulm/omi/cloudiator/lance/lca/container/port/DownstreamAddress.java
@@ -30,8 +30,7 @@ public final class DownstreamAddress{
 
 	private static boolean equalObjects(Object o1, Object o2) {
 		if(o1 == null && o2 == null) return true;
-		if(o1 == null && o2 != null) return false;
-		if(o1 != null && o2 == null) return false;
+		if(o1 == null || o2 == null) return false;
 		return o1.equals(o2);
 	}
 	
