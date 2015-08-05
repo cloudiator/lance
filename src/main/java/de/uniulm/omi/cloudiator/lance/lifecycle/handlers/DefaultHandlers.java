75f557c3ac6a4578be1b16bb9fddead5aa97434a
Joerg Domaschka <joerg.domaschka@uni-ulm.de>
2015-08-05 17:53:40 +0200

    removed System.out...

--- src/main/java/de/uniulm/omi/cloudiator/lance/lifecycle/handlers/DefaultHandlers.java
+++ src/main/java/de/uniulm/omi/cloudiator/lance/lifecycle/handlers/DefaultHandlers.java
@@ -1,70 +1,75 @@
 package de.uniulm.omi.cloudiator.lance.lifecycle.handlers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import de.uniulm.omi.cloudiator.lance.lifecycle.ExecutionContext;
 
 public final class DefaultHandlers {
+	
+	private static final Logger logger = LoggerFactory.getLogger(DefaultHandlers.class);
 
 	public static final InitHandler DEFAULT_INIT_HANDLER = new InitHandler() {
 
 		@Override public void execute(ExecutionContext ec) {
 			// throw new UnsupportedOperationException();
-			System.err.println("DEFAULT InitHandler doing nothing");
+			logger.info("DEFAULT InitHandler doing nothing");
 		}
 	}; 
 	
 	
 	public static final InstallHandler DEFAULT_INSTALL_HANDLER = new InstallHandler() {
 		@Override public void execute(ExecutionContext ec) {
-			System.err.println("DEFAULT InstallHandler doing nothing");
+			logger.info("DEFAULT InstallHandler doing nothing");
 		}
 	};
 	
 	public static final PostInstallHandler DEFAULT_POST_INSTALL_HANDLER = new PostInstallHandler() {
 		@Override public void execute(ExecutionContext ec) {
-			System.err.println("DEFAULT PostInstallHandler doing nothing");
+			logger.info("DEFAULT PostInstallHandler doing nothing");
 		}
 	};
 	
 
 	public static final PostStartHandler DEFAULT_POST_START_HANDLER = new PostStartHandler() {
 		@Override public void execute(ExecutionContext ec) {
-			System.err.println("DEFAULT PostStartHandler doing nothing");
+			logger.info("DEFAULT PostStartHandler doing nothing");
 		}		
 	};
 	
 	public static final PostStopHandler DEFAULT_POST_STOP_HANDLER = new PostStopHandler() {
 		@Override public void execute(ExecutionContext ec) {
-			System.err.println("DEFAULT PostStopHandler doing nothing");
+			logger.info("DEFAULT PostStopHandler doing nothing");
 		}
 	};
 	
 	public static final PreInstallHandler DEFAULT_PRE_INSTALL_HANDLER = new PreInstallHandler() {
 		@Override public void execute(ExecutionContext ec) {
-			System.err.println("DEFAULT PreInstallHandler doing nothing");
+			logger.info("DEFAULT PreInstallHandler doing nothing");
 		}
 	};
 	
 	public static final PreStartHandler DEFAULT_PRE_START_HANDLER = new PreStartHandler() {
 		@Override public void execute(ExecutionContext ec) {
-			System.err.println("DEFAULT PreStartHandler doing nothing");
+			logger.info("DEFAULT PreStartHandler doing nothing");
 		}
 	};	
 
 	public static final PreStopHandler DEFAULT_PRE_STOP_HANDLER = new PreStopHandler() {
 		@Override public void execute(ExecutionContext ec) {
-			System.err.println("DEFAULT PreStopHandler doing nothing");
+			logger.info("DEFAULT PreStopHandler doing nothing");
 		}
 	};
 	
 	public static final StartHandler DEFAULT_START_HANDLER = new StartHandler() {
 		@Override public void execute(ExecutionContext ec) {
-			System.err.println("DEFAULT StartHandler doing nothing");
+			logger.info("DEFAULT StartHandler doing nothing");
 		}
 	};
 	
 	public static final StopHandler DEFAULT_STOP_HANDLER = new StopHandler() {
 		@Override public void execute(ExecutionContext ec) {
-			System.err.println("DEFAULT StopHandler doing nothing");
+			logger.info("DEFAULT StopHandler doing nothing");
 		}
 	};
 	
