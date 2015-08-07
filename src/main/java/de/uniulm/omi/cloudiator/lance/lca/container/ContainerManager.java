29a8b3b71f07d8f80059ed3587c29ee0f173d983
Joerg Domaschka <joerg.domaschka@uni-ulm.de>
2015-08-07 12:04:06 +0200

    introduce exception when creating container.

--- src/main/java/de/uniulm/omi/cloudiator/lance/lca/container/ContainerManager.java
+++ src/main/java/de/uniulm/omi/cloudiator/lance/lca/container/ContainerManager.java
@@ -42,7 +42,8 @@ public interface ContainerManager {
     ContainerController getContainer(ComponentInstanceId id);
     void runApplication(ComponentInstanceId id, LifecycleStore store);
     CommandSequence addDeploymentSequence();
-    ContainerController createNewContainer(DeploymentContext ctx, DeployableComponent component, OperatingSystem os);
+    ContainerController createNewContainer(DeploymentContext ctx, DeployableComponent component, OperatingSystem os) throws ContainerException;
     void terminate();
 	List<ComponentInstanceId> getAllContainers();
+	ContainerStatus getComponentContainerStatus(ComponentInstanceId cid);
 }
