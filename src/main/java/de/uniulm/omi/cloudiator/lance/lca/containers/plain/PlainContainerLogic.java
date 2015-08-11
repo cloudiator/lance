214fa8a51eeed177d2af0217aa812f8ae504a17a
Daniel Seybold <daniel.seybold@uni-ulm.de>
2015-08-11 15:24:15 +0200

    PS-80 implemented getPortMapper for PlainContainerLogic

--- src/main/java/de/uniulm/omi/cloudiator/lance/lca/containers/plain/PlainContainerLogic.java
+++ src/main/java/de/uniulm/omi/cloudiator/lance/lca/containers/plain/PlainContainerLogic.java
@@ -1,9 +1,12 @@
 package de.uniulm.omi.cloudiator.lance.lca.containers.plain;
 
+import de.uniulm.omi.cloudiator.lance.application.DeploymentContext;
+import de.uniulm.omi.cloudiator.lance.application.component.InPort;
 import de.uniulm.omi.cloudiator.lance.container.standard.ContainerLogic;
 import de.uniulm.omi.cloudiator.lance.lca.container.ComponentInstanceId;
 import de.uniulm.omi.cloudiator.lance.lca.container.ContainerException;
 import de.uniulm.omi.cloudiator.lance.lca.container.port.InportAccessor;
+import de.uniulm.omi.cloudiator.lance.lca.container.port.PortRegistryTranslator;
 import de.uniulm.omi.cloudiator.lance.lifecycle.LifecycleActionInterceptor;
 import de.uniulm.omi.cloudiator.lance.lifecycle.LifecycleHandlerType;
 import de.uniulm.omi.cloudiator.lance.lifecycle.LifecycleStore;
@@ -12,6 +15,17 @@ import de.uniulm.omi.cloudiator.lance.lifecycle.LifecycleStore;
  * Created by Daniel Seybold on 10.08.2015.
  */
 public class PlainContainerLogic implements ContainerLogic, LifecycleActionInterceptor {
+
+    private final ComponentInstanceId myId;
+
+    private final DeploymentContext deploymentContext;
+
+    PlainContainerLogic(ComponentInstanceId id, DeploymentContext deploymentContext){
+
+        this.myId = id;
+        this.deploymentContext = deploymentContext;
+    }
+
     @Override
     public void doCreate() throws ContainerException {
         //prepare container env, e.g. create folders
@@ -37,9 +51,20 @@ public class PlainContainerLogic implements ContainerLogic, LifecycleActionInter
         return null;
     }
 
+    /**
+     * loops the port numbers through <a,b> -> <(a,a) , (b,b)>
+     * @return
+     */
     @Override
     public InportAccessor getPortMapper() {
-        return null;
+        return ( (portName, clientState) -> {
+
+                Integer portNumber = (Integer) deploymentContext.getProperty(portName, InPort.class);
+                clientState.registerValueAtLevel(PortRegistryTranslator.PORT_HIERARCHY_0, portNumber);
+                clientState.registerValueAtLevel(PortRegistryTranslator.PORT_HIERARCHY_1, portNumber);
+                clientState.registerValueAtLevel(PortRegistryTranslator.PORT_HIERARCHY_2, portNumber);
+
+        });
     }
 
     @Override
@@ -54,6 +79,6 @@ public class PlainContainerLogic implements ContainerLogic, LifecycleActionInter
 
     @Override
     public ComponentInstanceId getComponentId() {
-        return null;
+        return this.myId;
     }
 }
