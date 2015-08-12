b3d8015769d61d14fab99e8d16ec5f2308a038c5
Daniel Seybold <daniel.seybold@uni-ulm.de>
2015-08-12 11:17:41 +0200

    PS-80 implemented PlainShellImpl

--- src/main/java/de/uniulm/omi/cloudiator/lance/lca/containers/plain/shell/PlainShellImpl.java
+++ src/main/java/de/uniulm/omi/cloudiator/lance/lca/containers/plain/shell/PlainShellImpl.java
@@ -4,35 +4,38 @@ import de.uniulm.omi.cloudiator.lance.lifecycle.ExecutionResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by Daniel Seybold on 11.08.2015.
  */
 public class PlainShellImpl implements PlainShell {
-    //fixme: check if funtionality of InProgress fits better or use the Java ProcessBuilder
-    private Process shellProcess;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(PlainShell.class);
 
 
     @Override
     public ExecutionResult executeCommand(String command) {
         ExecutionResult executionResult = null;
+        Process shellProcess;
         try {
-            this.shellProcess = new ProcessBuilder(command).start();
+            shellProcess = new ProcessBuilder(command).inheritIO().start();
         } catch (IOException e) {
             LOGGER.error("Error while executing command: " + command,e);
-            //fixme: create failed executionResult
+            executionResult = ExecutionResult.systemFailure(e.getLocalizedMessage());
+            return executionResult;
         }
 
-        //fixme: create successfull execution result
+        executionResult = this.createExecutionResult(shellProcess);
 
         return executionResult;
     }
 
     @Override
     public ExecutionResult executeBlockingCommand(String command) {
-        //fixme: implement this in a blocking way
+        //fixme: implement this in a blocking way, check if blocking command are necessary
         return null;
     }
 
@@ -42,16 +45,66 @@ public class PlainShellImpl implements PlainShell {
         //fixme: implement this, check what needs to be closed or process killed?
     }
 
-    /*
-    private ExecutionResult checkProcess(){
-        int exitValue = this.shellProcess.exitValue();
+    private ExecutionResult createExecutionResult(Process process){
+
         ExecutionResult executionResult;
+
+        int exitValue = process.exitValue();
+
         switch (exitValue){
-            case 0: ExecutionResult.success(this.shellProcess.getOutputStream()., this.shellProcess.getErrorStream());
-            case -1:;
+            case 0:
+                executionResult = ExecutionResult.success(extractCommandOutput(process),extractErrorOutput(process));
             default:
+                executionResult = ExecutionResult.commandFailure(exitValue, extractCommandOutput(process),extractErrorOutput(process));
+
         }
+
         return executionResult;
+
     }
-    */
+
+    private static String extractCommandOutput(Process process){
+        String output;
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        StringBuilder builder = new StringBuilder();
+        String line = null;
+        try {
+            while ( (line = reader.readLine()) != null) {
+                builder.append(line);
+                builder.append(System.getProperty("line.separator"));
+            }
+        } catch (IOException e) {
+            LOGGER.error("Error while reading process outputstream", e);
+            e.printStackTrace();
+        }
+
+        output = builder.toString();
+
+        return output;
+    }
+
+    private static String extractErrorOutput(Process process){
+        String output;
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+        StringBuilder builder = new StringBuilder();
+        String line = null;
+        try {
+            while ( (line = reader.readLine()) != null) {
+                builder.append(line);
+                builder.append(System.getProperty("line.separator"));
+            }
+        } catch (IOException e) {
+            LOGGER.error("Error while reading process errorstream", e);
+            e.printStackTrace();
+        }
+
+        output = builder.toString();
+
+        return output;
+    }
+
+
+
 }
