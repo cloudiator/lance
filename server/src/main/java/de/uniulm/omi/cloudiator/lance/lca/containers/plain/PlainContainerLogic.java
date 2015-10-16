d782dcc1ca0bc4301c470a6abd440ebd2887cc57
Daniel Seybold <daniel.seybold@uni-ulm.de>
2015-10-16 11:01:54 +0200

    PS-99 fixed file sperator to use Java System file seperator

--- server/src/main/java/de/uniulm/omi/cloudiator/lance/lca/containers/plain/PlainContainerLogic.java
+++ server/src/main/java/de/uniulm/omi/cloudiator/lance/lca/containers/plain/PlainContainerLogic.java
@@ -74,8 +74,9 @@ public class PlainContainerLogic implements ContainerLogic, LifecycleActionInter
         LOGGER.info("creating new plain container with foldername " + this.myId.toString());
         plainShell.executeCommand("mkdir " + this.myId.toString());
 
-        LOGGER.info("Switching to plain container: " + System.getProperty("user.dir") + "\\" + this.myId.toString());
-        plainShell.setDirectory(System.getProperty("user.dir") + "\\" + this.myId.toString());
+        String createFolderCommand = System.getProperty("user.dir") + System.getProperty("file.separator") + this.myId.toString();
+        LOGGER.info("Switching to plain container: " + createFolderCommand);
+        plainShell.setDirectory(createFolderCommand);
 
         //installing shell
         this.plainShellFactory.installPlainShell(plainShell);
