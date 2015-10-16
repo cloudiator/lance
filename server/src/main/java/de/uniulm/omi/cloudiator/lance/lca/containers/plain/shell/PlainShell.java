ea4716675743e4e7b008c3e46268c65d2bdb2383
Daniel Baur <daniel.baur@uni-ulm.de>
2015-10-16 15:21:59 +0200

    Changed command wrapping in plain shell implementation

--- server/src/main/java/de/uniulm/omi/cloudiator/lance/lca/containers/plain/shell/PlainShell.java
+++ server/src/main/java/de/uniulm/omi/cloudiator/lance/lca/containers/plain/shell/PlainShell.java
@@ -21,14 +21,10 @@ package de.uniulm.omi.cloudiator.lance.lca.containers.plain.shell;
 import de.uniulm.omi.cloudiator.lance.lca.container.environment.ShellLikeInterface;
 import de.uniulm.omi.cloudiator.lance.lifecycle.ExecutionResult;
 
-import java.io.File;
-
 /**
  * Created by Daniel Seybold on 11.08.2015.
  */
 public interface PlainShell extends ShellLikeInterface {
-
-    public ExecutionResult executeCommand(String command);
 
     public ExecutionResult executeBlockingCommand(String command);
 
@@ -36,5 +32,4 @@ public interface PlainShell extends ShellLikeInterface {
 
     public void setDirectory(String directory);
 
-    public void setEnvVar(String key, String value);
 }
--- server/src/main/java/de/uniulm/omi/cloudiator/lance/lca/containers/plain/shell/PlainShellImpl.java
+++ server/src/main/java/de/uniulm/omi/cloudiator/lance/lca/containers/plain/shell/PlainShellImpl.java
@@ -22,14 +22,13 @@ import de.uniulm.omi.cloudiator.lance.container.spec.os.OperatingSystem;
 import de.uniulm.omi.cloudiator.lance.lifecycle.ExecutionResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.rmi.runtime.Log;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Created by Daniel Seybold on 11.08.2015.
@@ -38,37 +37,36 @@ public class PlainShellImpl implements PlainShell {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PlainShell.class);
 
-    private  ProcessBuilder processBuilder = new ProcessBuilder();
+    private ProcessBuilder processBuilder = new ProcessBuilder();
     //private ProcessBuilder processBuilder = new ProcessBuilder().inheritIO();
 
     private final List<String> osShell = new ArrayList<String>();
 
-    public PlainShellImpl(OperatingSystem operatingSystem){
+    public PlainShellImpl(OperatingSystem operatingSystem) {
 
         //fixme: do this in a more generic way
         //add the os respective shells for execution
-        if(operatingSystem.equals(OperatingSystem.WINDOWS_7)){
+        if (operatingSystem.equals(OperatingSystem.WINDOWS_7)) {
 
             this.osShell.add("powershell.exe");
             this.osShell.add("-command");
 
-        }else if(operatingSystem.equals(OperatingSystem.UBUNTU_14_04)){
+        } else if (operatingSystem.equals(OperatingSystem.UBUNTU_14_04)) {
 
             this.osShell.add("/bin/bash");
             this.osShell.add("-c");
 
-        }else{
+        } else {
             throw new IllegalStateException("Unkown OS: " + operatingSystem.toString());
         }
     }
 
 
-    @Override
-    public ExecutionResult executeCommand(String command) {
-        ExecutionResult executionResult = null;
+    @Override public ExecutionResult executeCommand(String command) {
+        ExecutionResult executionResult;
         Process shellProcess;
 
-        List<String>commands = this.buildCommand(command);
+        List<String> commands = this.buildCommand(command);
 
         try {
 
@@ -81,24 +79,24 @@ public class PlainShellImpl implements PlainShell {
                 //System.out.println("Content: " + string);
             });
 
-            String commandOut = this.extractCommandOutput(shellProcess);
+            String commandOut = extractCommandOutput(shellProcess);
             //System.out.println(commandOut); //debugging
             LOGGER.debug(commandOut);
 
-            String errorOut = this.extractErrorOutput(shellProcess);
+            String errorOut = extractErrorOutput(shellProcess);
             //System.out.println(errorOut); //debugging
             LOGGER.debug(errorOut);
 
 
             //important, wait for or it will run in an deadlock!!, adapt execution result maybe
-            int exitValue =shellProcess.waitFor();
+            int exitValue = shellProcess.waitFor();
             LOGGER.debug("ExitCode: " + exitValue);
 
             executionResult = this.createExecutionResult(exitValue, commandOut, errorOut);
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
-            LOGGER.error("Error while executing command: " + command,e);
+            LOGGER.error("Error while executing command: " + command, e);
             executionResult = ExecutionResult.systemFailure(e.getLocalizedMessage());
             return executionResult;
         }
@@ -107,83 +105,70 @@ public class PlainShellImpl implements PlainShell {
         return executionResult;
     }
 
-    private List<String> buildCommand(String commandLine){
-
-        String[] splittedCommands = this.splitCommandLines(commandLine);
+    private List<String> buildCommand(String commandLine) {
 
         List<String> commandList = new ArrayList<String>();
-        Matcher m = Pattern.compile("([^\"]\\S*|\".+?\")\\s*").matcher(commandLine);
-        while (m.find())
-            commandList.add(m.group(1)); // Add .replace("\"", "") to remove surrounding quotes.
+        //Matcher m = Pattern.compile("([^\"]\\S*|\".+?\")\\s*").matcher(commandLine);
+        //while (m.find())
+        //    commandList.add(m.group(1)); // Add .replace("\"", "") to remove surrounding quotes.
+
 
 
         //create list with os specific commands
-        List<String> result = new ArrayList<String>(this.osShell);
+        commandList.addAll(this.osShell);
+
+        //todo check if already wrapped
+        commandList.add("\"" + commandLine + "\"");
 
         //add app commands and wrap them in double quotes, single quotes won't work on Windows
-        result.add("\"");
-        result.addAll(commandList);
-        result.add("\"");
+        //result.add("\"");
+        //result.addAll(commandList);
+        //result.add("\"");
 
-        return result;
-
+        return commandList;
     }
 
-    private String[] splitCommandLines(String commandLine){
-        String[] commands = commandLine.split(" ");
-
-        return commands;
-    }
-
-    @Override
-    public ExecutionResult executeBlockingCommand(String command) {
+    @Override public ExecutionResult executeBlockingCommand(String command) {
         //fixme: implement this in a blocking way, check if blocking command are necessary
         LOGGER.warn("Using currently same impl for blocking/nonblocking execution of commands!");
         return this.executeCommand(command);
     }
 
 
-    @Override
-    public void close() {
+    @Override public void close() {
         //fixme: implement this, check what needs to be closed or process killed?
         LOGGER.warn("Closing PlainShellImpl needs to be implemented!");
     }
 
-    @Override
-    public void setDirectory(String directory) {
+    @Override public void setDirectory(String directory) {
         this.processBuilder.directory(new File(directory));
 
         LOGGER.info(this.processBuilder.directory().getAbsoluteFile().toString());
     }
 
-    @Override
-    public void setEnvVar(String key, String value) {
-
-    }
-
-
-    private ExecutionResult createExecutionResult(int exitValue, String commandOut, String errorOut){
+    private ExecutionResult createExecutionResult(int exitValue, String commandOut,
+        String errorOut) {
 
         ExecutionResult executionResult;
 
-        if(exitValue == 0){
-            executionResult = ExecutionResult.success(commandOut,errorOut);
-        }else{
-            executionResult = ExecutionResult.commandFailure(exitValue, commandOut,errorOut);
+        if (exitValue == 0) {
+            executionResult = ExecutionResult.success(commandOut, errorOut);
+        } else {
+            executionResult = ExecutionResult.commandFailure(exitValue, commandOut, errorOut);
         }
 
         return executionResult;
 
     }
 
-    private static String extractCommandOutput(Process process){
+    private static String extractCommandOutput(Process process) {
         String output;
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         StringBuilder builder = new StringBuilder();
-        String line = null;
+        String line;
         try {
-            while ( (line = reader.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
                 builder.append(line);
                 builder.append(System.getProperty("line.separator"));
             }
@@ -198,18 +183,17 @@ public class PlainShellImpl implements PlainShell {
 
 
 
-
         return output;
     }
 
-    private static String extractErrorOutput(Process process){
+    private static String extractErrorOutput(Process process) {
         String output;
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
         StringBuilder builder = new StringBuilder();
-        String line = null;
+        String line;
         try {
-            while ( (line = reader.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
                 builder.append(line);
                 builder.append(System.getProperty("line.separator"));
             }
