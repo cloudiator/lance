1dadf93df91f9495a7f15c620e25165972c37b9a
Daniel Baur <daniel.baur@uni-ulm.de>
2015-10-16 16:22:18 +0200

    Added hostcontext to PlainContainerLogic, fixed get local address method

--- server/src/main/java/de/uniulm/omi/cloudiator/lance/lca/containers/plain/PlainContainerLogic.java
+++ server/src/main/java/de/uniulm/omi/cloudiator/lance/lca/containers/plain/PlainContainerLogic.java
@@ -23,6 +23,7 @@ import de.uniulm.omi.cloudiator.lance.application.component.DeployableComponent;
 import de.uniulm.omi.cloudiator.lance.application.component.InPort;
 import de.uniulm.omi.cloudiator.lance.container.spec.os.OperatingSystem;
 import de.uniulm.omi.cloudiator.lance.container.standard.ContainerLogic;
+import de.uniulm.omi.cloudiator.lance.lca.HostContext;
 import de.uniulm.omi.cloudiator.lance.lca.container.ComponentInstanceId;
 import de.uniulm.omi.cloudiator.lance.lca.container.ContainerException;
 import de.uniulm.omi.cloudiator.lance.lca.container.environment.BashExportBasedVisitor;
@@ -38,9 +39,6 @@ import de.uniulm.omi.cloudiator.lance.lifecycle.LifecycleStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 /**
  * Created by Daniel Seybold on 10.08.2015.
  */
@@ -54,9 +52,11 @@ public class PlainContainerLogic implements ContainerLogic, LifecycleActionInter
     private final OperatingSystem os;
     private final NetworkHandler networkHandler;
     private final PlainShellFactory plainShellFactory;
+    private final HostContext hostContext;
 
-    PlainContainerLogic(ComponentInstanceId id, DeployableComponent deployableComponent, DeploymentContext deploymentContext,
-                        OperatingSystem os, NetworkHandler networkHandler, PlainShellFactory plainShellFactory){
+    PlainContainerLogic(ComponentInstanceId id, DeployableComponent deployableComponent,
+        DeploymentContext deploymentContext, OperatingSystem os, NetworkHandler networkHandler,
+        PlainShellFactory plainShellFactory, HostContext hostContext) {
 
         this.myId = id;
         this.deployableComponent = deployableComponent;
@@ -64,17 +64,19 @@ public class PlainContainerLogic implements ContainerLogic, LifecycleActionInter
         this.os = os;
         this.networkHandler = networkHandler;
         this.plainShellFactory = plainShellFactory;
+        this.hostContext = hostContext;
     }
 
-    @Override
-    public void doCreate() throws ContainerException {
+    @Override public void doCreate() throws ContainerException {
         LOGGER.info("Creating shell for operating system: " + this.os.toString());
         PlainShell plainShell = new PlainShellImpl(this.os);
 
         LOGGER.info("creating new plain container with foldername " + this.myId.toString());
         plainShell.executeCommand("mkdir " + this.myId.toString());
 
-        String switchFolderCommand = System.getProperty("user.dir") + System.getProperty("file.separator") + this.myId.toString();
+        String switchFolderCommand =
+            System.getProperty("user.dir") + System.getProperty("file.separator") + this.myId
+                .toString();
         LOGGER.info("Switching to plain container: " + switchFolderCommand);
         plainShell.setDirectory(switchFolderCommand);
 
@@ -83,52 +85,49 @@ public class PlainContainerLogic implements ContainerLogic, LifecycleActionInter
 
     }
 
-    @Override
-    public void doInit(LifecycleStore store) throws ContainerException {
+    @Override public void doInit(LifecycleStore store) throws ContainerException {
         //probably not needed for plain container
 
     }
 
 
-    @Override
-    public void completeInit() throws ContainerException {
+    @Override public void completeInit() throws ContainerException {
 
         this.plainShellFactory.closeShell();
     }
 
-    @Override
-    public void doDestroy() throws ContainerException {
+    @Override public void doDestroy() throws ContainerException {
         throw new UnsupportedOperationException();
     }
 
-    @Override
-    public String getLocalAddress() throws ContainerException {
+    @Override public String getLocalAddress() throws ContainerException {
+
+        return hostContext.getInternalIp();
 
         //not used in Plain Container
-        String result = null;
+        // String result = null;
 
-        return result;
+        //return result;
     }
 
     /**
      * loops the port numbers through <a,b> -> <(a,a) , (b,b)>
+     *
      * @return
      */
-    @Override
-    public InportAccessor getPortMapper() {
-        return ( (portName, clientState) -> {
+    @Override public InportAccessor getPortMapper() {
+        return ((portName, clientState) -> {
 
-                Integer portNumber = (Integer) deploymentContext.getProperty(portName, InPort.class);
-                clientState.registerValueAtLevel(PortRegistryTranslator.PORT_HIERARCHY_0, portNumber);
-                clientState.registerValueAtLevel(PortRegistryTranslator.PORT_HIERARCHY_1, portNumber);
-                clientState.registerValueAtLevel(PortRegistryTranslator.PORT_HIERARCHY_2, portNumber);
+            Integer portNumber = (Integer) deploymentContext.getProperty(portName, InPort.class);
+            clientState.registerValueAtLevel(PortRegistryTranslator.PORT_HIERARCHY_0, portNumber);
+            clientState.registerValueAtLevel(PortRegistryTranslator.PORT_HIERARCHY_1, portNumber);
+            clientState.registerValueAtLevel(PortRegistryTranslator.PORT_HIERARCHY_2, portNumber);
 
         });
     }
 
-    @Override
-    public void prepare(LifecycleHandlerType type) {
-        if(type == LifecycleHandlerType.INSTALL) {
+    @Override public void prepare(LifecycleHandlerType type) {
+        if (type == LifecycleHandlerType.INSTALL) {
             preInstallAction();
         }
 
@@ -138,29 +137,30 @@ public class PlainContainerLogic implements ContainerLogic, LifecycleActionInter
         PlainShellWrapper plainShellWrapper = this.plainShellFactory.createShell();
 
         //todo: move os switch to a central point (currently here and in PlainShellImpl)
-        if(this.os.equals(OperatingSystem.WINDOWS_7)){
+        if (this.os.equals(OperatingSystem.WINDOWS_7)) {
 
-            PowershellExportBasedVisitor visitor = new PowershellExportBasedVisitor(plainShellWrapper.plainShell);
+            PowershellExportBasedVisitor visitor =
+                new PowershellExportBasedVisitor(plainShellWrapper.plainShell);
             networkHandler.accept(visitor);
             this.deployableComponent.accept(this.deploymentContext, visitor);
 
-        }else if(this.os.equals(OperatingSystem.UBUNTU_14_04)){
-            BashExportBasedVisitor visitor = new BashExportBasedVisitor(plainShellWrapper.plainShell);
+        } else if (this.os.equals(OperatingSystem.UBUNTU_14_04)) {
+            BashExportBasedVisitor visitor =
+                new BashExportBasedVisitor(plainShellWrapper.plainShell);
 
             networkHandler.accept(visitor);
             this.deployableComponent.accept(this.deploymentContext, visitor);
 
-        }else{
+        } else {
             throw new RuntimeException("Unsupported Operating System: " + this.os.toString());
         }
 
     }
 
-    @Override
-    public void postprocess(LifecycleHandlerType type) {
-        if(type == LifecycleHandlerType.PRE_INSTALL) {
+    @Override public void postprocess(LifecycleHandlerType type) {
+        if (type == LifecycleHandlerType.PRE_INSTALL) {
             postPreInstall();
-        } else if(type == LifecycleHandlerType.POST_INSTALL) {
+        } else if (type == LifecycleHandlerType.POST_INSTALL) {
             // TODO: how should we snapshot the folder? //
         }
     }
@@ -169,8 +169,7 @@ public class PlainContainerLogic implements ContainerLogic, LifecycleActionInter
         //FIXME: not necessary for plain container
     }
 
-    @Override
-    public ComponentInstanceId getComponentId() {
+    @Override public ComponentInstanceId getComponentId() {
         return this.myId;
     }
 }
