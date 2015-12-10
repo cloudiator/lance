6b8a4415f80de77479d7756818bddc7f4787dbf9
Joerg Domaschka <joerg.domaschka@uni-ulm.de>
2015-12-10 08:47:45 +0100

    refacto LifecycleController; outsource boilerplate code.

--- server/src/main/java/de/uniulm/omi/cloudiator/lance/lifecycle/LifecycleController.java
+++ server/src/main/java/de/uniulm/omi/cloudiator/lance/lifecycle/LifecycleController.java
@@ -28,17 +28,8 @@ import de.uniulm.omi.cloudiator.lance.lca.container.port.DownstreamAddress;
 import de.uniulm.omi.cloudiator.lance.lca.container.port.PortDiff;
 import de.uniulm.omi.cloudiator.lance.lca.registry.RegistrationException;
 import de.uniulm.omi.cloudiator.lance.lifecycle.detector.PortUpdateHandler;
-import de.uniulm.omi.cloudiator.lance.lifecycle.handlers.InitHandler;
-import de.uniulm.omi.cloudiator.lance.lifecycle.handlers.InstallHandler;
-import de.uniulm.omi.cloudiator.lance.lifecycle.handlers.PostInstallHandler;
-import de.uniulm.omi.cloudiator.lance.lifecycle.handlers.PostStartHandler;
-import de.uniulm.omi.cloudiator.lance.lifecycle.handlers.PreInstallHandler;
-import de.uniulm.omi.cloudiator.lance.lifecycle.handlers.PreStartHandler;
-import de.uniulm.omi.cloudiator.lance.lifecycle.handlers.StartHandler;
-import de.uniulm.omi.cloudiator.lance.util.state.StateMachine;
-import de.uniulm.omi.cloudiator.lance.util.state.StateMachineBuilder;
-import de.uniulm.omi.cloudiator.lance.util.state.TransitionAction;
 
+import de.uniulm.omi.cloudiator.lance.util.state.StateMachine;
 public final class LifecycleController {
     
     private static final Logger LOGGER = LoggerFactory.getLogger(LifecycleController.class);
@@ -57,7 +48,7 @@ public final class LifecycleController {
             GlobalRegistryAccessor accessorParam, ExecutionContext ecParam) {
         store = storeParam;
         ec = ecParam;
-        machine = buildStateMachine();
+        machine = LifecycleControllerTransitions.buildStateMachine(store, ec);
         interceptor = interceptorParam; 
         accessor = accessorParam;
         
@@ -114,11 +105,14 @@ public final class LifecycleController {
     
     public synchronized void blockingStart() {
         run(LifecycleHandlerType.PRE_START);    // moves to START and calls 'start handler'
-        // FIXME: establish start detector
-        // machine.transit(LifecycleHandlerType.START);        // moves to POST_START
+        // FIXME: establish start detectors
+        getLogger().warn("TODO: run start detector");
+        // FIXME: establish stop detector
+        getLogger().warn("TODO: run start detector");
+        machine.transit(LifecycleHandlerType.START);        // moves to POST_START
     }
     
-    public synchronized void blockingUpdatePorts(OutPort port, PortUpdateHandler handler, PortDiff<DownstreamAddress> diff) throws ContainerException {
+    public synchronized void blockingUpdatePorts(@SuppressWarnings("unused") OutPort port, PortUpdateHandler handler, PortDiff<DownstreamAddress> diff) throws ContainerException {
 	    try {
 	    	interceptor.preprocessPortUpdate(diff);
 	    	LOGGER.warn("updating ports via port handler.");
@@ -132,72 +126,5 @@ public final class LifecycleController {
 		}
     }
 
-    private StateMachine<LifecycleHandlerType> buildStateMachine() {
-        return addInitTransition(
-                addInstallTransitions(
-                        addStartTransitions(
-                                new  StateMachineBuilder<>(LifecycleHandlerType.NEW).
-                                addAllState(LifecycleHandlerType.values())
-                    ))).build();
-    }
-    
-    private StateMachineBuilder<LifecycleHandlerType> addInitTransition(StateMachineBuilder<LifecycleHandlerType> b) {
-        return b.addSynchronousTransition(LifecycleHandlerType.NEW, LifecycleHandlerType.INIT,
-                new TransitionAction() {
-                    @Override public void transit(Object[] params) {
-                        InitHandler h = store.getHandler(LifecycleHandlerType.INIT, InitHandler.class);
-                        h.execute(ec);
-                    }
-                });
-    }
-    
-    private StateMachineBuilder<LifecycleHandlerType> addInstallTransitions(StateMachineBuilder<LifecycleHandlerType> b) {
-        return b.addSynchronousTransition(LifecycleHandlerType.INIT, LifecycleHandlerType.PRE_INSTALL,
-                new TransitionAction() {
-                    @Override public void transit(Object[] params) {
-                        PreInstallHandler h = store.getHandler(LifecycleHandlerType.PRE_INSTALL, PreInstallHandler.class);
-                        h.execute(ec);
-                    }
-                }).
-                addSynchronousTransition(LifecycleHandlerType.PRE_INSTALL, LifecycleHandlerType.INSTALL, 
-                    new TransitionAction() {
-                        @Override public void transit(Object[] params) {
-                            InstallHandler h = store.getHandler(LifecycleHandlerType.INSTALL, InstallHandler.class);
-                            h.execute(ec);
-                        }
-                }). 
-                addSynchronousTransition(LifecycleHandlerType.INSTALL, LifecycleHandlerType.POST_INSTALL, 
-                    new TransitionAction() {
-                        @Override public void transit(Object[] params) {
-                            PostInstallHandler h = store.getHandler(LifecycleHandlerType.POST_INSTALL, PostInstallHandler.class);
-                            h.execute(ec);
-                        }
-                });        
-    }
-    
-    private StateMachineBuilder<LifecycleHandlerType> addStartTransitions(StateMachineBuilder<LifecycleHandlerType> b) {
-        return b.addSynchronousTransition(LifecycleHandlerType.POST_INSTALL, LifecycleHandlerType.PRE_START, 
-                new TransitionAction() {
-                    @Override public void transit(Object[] params) {
-                            PreStartHandler h = store.getHandler(LifecycleHandlerType.PRE_START, PreStartHandler.class);
-                            h.execute(ec);
-                    }
-                }).
-                addSynchronousTransition(LifecycleHandlerType.PRE_START, LifecycleHandlerType.START,
-                    new TransitionAction() {
-                        @Override public void transit(Object[] params) {
-                            StartHandler h = store.getHandler(LifecycleHandlerType.START, StartHandler.class);
-                            h.execute(ec);
-                            // FIXME: run start detector and stop detector //
-                            getLogger().warn("WARNING: run start detector and stop detector");
-                        }
-                }). 
-                addSynchronousTransition(LifecycleHandlerType.START, LifecycleHandlerType.POST_START,
-                    new TransitionAction() {
-                        @Override public void transit(Object[] params) {
-                            PostStartHandler h = store.getHandler(LifecycleHandlerType.POST_START, PostStartHandler.class);
-                            h.execute(ec);
-                    }
-                });
-    }
+
 }
