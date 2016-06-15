385960c6e920e385222b84a6c795511f904bb1af
Jörg Domaschka <joerg.domaschka@uni-ulm.de>
2016-06-15 21:36:25 +0200

    more testability to shells.

---
+++ server/src/main/java/de/uniulm/omi/cloudiator/lance/lca/containers/plain/PlainShellFactoryImpl.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2014-2015 University of Ulm
+ *
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.  Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package de.uniulm.omi.cloudiator.lance.lca.containers.plain;
+
+import de.uniulm.omi.cloudiator.lance.container.spec.os.OperatingSystem;
+import de.uniulm.omi.cloudiator.lance.lca.containers.plain.shell.PlainShell;
+import de.uniulm.omi.cloudiator.lance.lca.containers.plain.shell.PlainShellImpl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * Created by Daniel Seybold on 12.08.2015.
+ */
+final class PlainShellFactoryImpl implements PlainShellFactory {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PlainShell.class);
+
+    private final AtomicReference<PlainShellWrapper> reference = new AtomicReference<>();
+
+    @Override
+    public PlainShellWrapper createShell() {
+        PlainShellWrapper wrapper = reference.get();
+        if(wrapper == null)
+            throw new IllegalStateException("plain shell not set");
+        return wrapper;
+    }
+
+    @Override
+    public PlainShell createAndinstallPlainShell(OperatingSystem os) {
+        PlainShell plainShell = new PlainShellImpl(os);
+        final PlainShellWrapper wrapper = new PlainShellWrapper(plainShell);
+        PlainShellWrapper old = reference.getAndSet(wrapper);
+        if(old != null) {
+            LOGGER.error("ERROR: overriding plain shell with new one. this should never happen.");
+        }
+        return plainShell;
+    }
+
+    @Override
+    public void closeShell() {
+        PlainShellWrapper old = reference.getAndSet(null);
+        if(old == null) {
+            LOGGER.error("ERROR: no plain shell set that can be closed.");
+        } else {
+            old.plainShell.close();
+        }
+    }
+}
---
+++ server/src/test/de/uniulm/omi/cloudiator/lance/lca/containers/plain/TestPlainShellFactory.java
@@ -0,0 +1,85 @@
+package de.uniulm.omi.cloudiator.lance.lca.containers.plain;
+
+import java.util.Stack;
+import java.util.concurrent.atomic.AtomicReference;
+
+import de.uniulm.omi.cloudiator.lance.container.spec.os.OperatingSystem;
+import de.uniulm.omi.cloudiator.lance.lca.containers.plain.shell.PlainShell;
+import de.uniulm.omi.cloudiator.lance.lca.containers.plain.shell.PlainShellImpl;
+import de.uniulm.omi.cloudiator.lance.lifecycle.ExecutionResult;
+
+final class TestPlainShellFactory implements PlainShellFactory {
+
+	private final Stack<TestPlainShell> shells = new Stack<>();
+    private final AtomicReference<PlainShellWrapper> reference = new AtomicReference<>();
+
+	@Override
+	public PlainShellWrapper createShell() {
+		PlainShellWrapper wrapper = reference.get();
+		if(wrapper == null)
+			throw new IllegalStateException("plain shell not set");
+		return wrapper;
+	}
+
+	@Override
+	public void closeShell() {
+        PlainShellWrapper old = reference.getAndSet(null);
+        if(old == null) {
+            throw new IllegalStateException("shell was not be set");
+        } else {
+            old.plainShell.close();
+        }
+	}
+
+	@Override
+	public PlainShell createAndinstallPlainShell(OperatingSystem os) {
+		TestPlainShell plainShell = new TestPlainShell(os);
+		shells.push(plainShell);
+        final PlainShellWrapper wrapper = new PlainShellWrapper(plainShell);
+        PlainShellWrapper old = reference.getAndSet(wrapper);
+        if(old != null) {
+        	throw new IllegalStateException("has never been set");
+        }
+        return plainShell;
+	}
+
+	static class TestPlainShell implements PlainShell {
+
+		private final OperatingSystem os;
+		
+		public TestPlainShell(OperatingSystem osP) {
+			os = osP;
+		}
+
+		@Override
+		public ExecutionResult executeCommand(String string) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void setEnvironmentVariable(String key, String value) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public ExecutionResult executeBlockingCommand(String command) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void close() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void setDirectory(String directory) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+}
