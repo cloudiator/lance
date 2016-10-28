b9b0babc8105debdf605ec01ec451cf85fd7d2fb
Daniel Baur <daniel.baur@uni-ulm.de>
2016-10-28 10:19:04 +0200

    PS-212 server stub is now static

--- server/src/main/java/de/uniulm/omi/cloudiator/lance/lca/LifecycleAgentBooter.java
+++ server/src/main/java/de/uniulm/omi/cloudiator/lance/lca/LifecycleAgentBooter.java
@@ -18,29 +18,27 @@
 
 package de.uniulm.omi.cloudiator.lance.lca;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import de.uniulm.omi.cloudiator.lance.LcaConstants;
-import de.uniulm.omi.cloudiator.lance.lca.LifecycleAgent;
 import de.uniulm.omi.cloudiator.lance.util.Registrator;
 import de.uniulm.omi.cloudiator.lance.util.Version;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class LifecycleAgentBooter {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(LifecycleAgentBooter.class);
     private final static Registrator<LifecycleAgent> reg = Registrator.create(LifecycleAgent.class);
-    private static volatile LifecycleAgentImpl lca;
-    
+    private static volatile LifecycleAgentImpl lca = createAgentImplementation();
+    private static volatile LifecycleAgent stub = reg.export(lca, LcaConstants.AGENT_RMI_PORT);
+
     public static void main(String[] args) {
-        LOGGER.info("LifecycleAgentBooter: starting. running version: " + Version.getVersionString());
-        lca = createAgentImplementation();
-        
-        LifecycleAgent stub = reg.export(lca, LcaConstants.AGENT_RMI_PORT);
+        LOGGER
+            .info("LifecycleAgentBooter: starting. running version: " + Version.getVersionString());
+
         // TODO: it might be worth exploiting ways to get rid of this
         // dependency to a registry. note that there does not seem to
         // be an easy way to do it (i.e. relaying on standard interfaces)
-        if(stub != null && reg.addToRegistry(stub, LcaConstants.AGENT_REGISTRY_KEY)) {
+        if (stub != null && reg.addToRegistry(stub, LcaConstants.AGENT_REGISTRY_KEY)) {
             // from here on RMI takes over //
             LOGGER.info("LifecycleAgentBooter: agent exported. waiting for requests.");
         } else {
@@ -48,7 +46,7 @@ public final class LifecycleAgentBooter {
             Runtime.getRuntime().exit(-128);
         }
     }
-    
+
     private static LifecycleAgentImpl createAgentImplementation() {
         HostContext ctx = EnvContext.fromEnvironment();
         LOGGER.info("LifecycleAgentBooter: created host context: " + ctx);
@@ -57,10 +55,10 @@ public final class LifecycleAgentBooter {
         return impl;
     }
 
-	public static void unregister(LifecycleAgentImpl lifecycleAgentImpl) {
-		reg.unregister(lifecycleAgentImpl);
-	}
-    
-    
+    public static void unregister(LifecycleAgentImpl lifecycleAgentImpl) {
+        reg.unregister(lifecycleAgentImpl);
+    }
+
+
 
 }
