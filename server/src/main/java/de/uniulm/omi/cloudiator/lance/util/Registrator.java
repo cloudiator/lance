8865c583615e8aa9a70958984b0595993f79cba8
Daniel Baur <daniel.baur@uni-ulm.de>
2016-10-28 11:17:01 +0200

    PS-212 rmi registry is now static

--- server/src/main/java/de/uniulm/omi/cloudiator/lance/util/Registrator.java
+++ server/src/main/java/de/uniulm/omi/cloudiator/lance/util/Registrator.java
@@ -18,103 +18,105 @@
 
 package de.uniulm.omi.cloudiator.lance.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.rmi.NoSuchObjectException;
 import java.rmi.NotBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.concurrent.atomic.AtomicReference;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public final class Registrator<T extends Remote> {
-	
-	public static final <S extends Remote> Registrator<S> create(Class<S> clazz) {
-		return new Registrator<>(clazz);
-	}
-	
-	private final static Logger LOGGER = LoggerFactory.getLogger(Registrator.class);
-    private final AtomicReference<Registry> the_registry = new AtomicReference<>();
-	private final Class<T> myClass;
-	
-	private Registrator (Class<T> classParam) {
-		myClass = classParam;
-	}
-	
-	public <S extends T> T export(S object, int port) {
+
+    public static <S extends Remote> Registrator<S> create(Class<S> clazz) {
+        return new Registrator<>(clazz);
+    }
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(Registrator.class);
+    private static volatile Registry the_registry = createRegistry();
+    private final Class<T> myClass;
+
+    private Registrator(Class<T> classParam) {
+        myClass = classParam;
+    }
+
+    public <S extends T> T export(S object, int port) {
         assert object != null : "object must not be null";
         LOGGER.info("exporting object " + object + " to registry on port " + port);
 
         try {
             return (T) UnicastRemoteObject.exportObject(object, port);
-        } catch(RemoteException re) {
+        } catch (RemoteException re) {
             LOGGER.error("got exception at object export; quitting the platform", re);
         }
         return null;
     }
-    
+
     public boolean addToRegistry(T object, String key) {
         LOGGER.info("adding object to registry with key " + key);
         try {
-            Registry reg = getAndCreateRegistry();
-            removeExisting(reg, key);
-            reg.rebind(key, object);
+            removeExisting(the_registry, key);
+            the_registry.rebind(key, object);
             return true;
-        } catch(RemoteException e) { // includes AccessException //
-            LOGGER.error("got exception at startup: could not add object to registry. aborting.", e);
-        } 
+        } catch (RemoteException e) { // includes AccessException //
+            LOGGER
+                .error("got exception at startup: could not add object to registry. aborting.", e);
+        }
         return false;
     }
-    
-    private synchronized Registry getAndCreateRegistry() throws RemoteException {
+
+    private static Registry createRegistry() {
         LOGGER.info("creating rmi registry");
-        Registry reg = the_registry.get();
-        if(reg != null) return reg;
+        Registry reg;
         try {
             reg = java.rmi.registry.LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
-        } catch(RemoteException re) {
+        } catch (RemoteException re) {
             LOGGER.info("could not create local registry. assuming, it already exists.");
-            reg = java.rmi.registry.LocateRegistry.getRegistry(Registry.REGISTRY_PORT);
+            try {
+                reg = java.rmi.registry.LocateRegistry.getRegistry(Registry.REGISTRY_PORT);
+            } catch (RemoteException e) {
+                throw new RuntimeException(
+                    "could not create and retrieve local registry, failing...");
+            }
         }
-        the_registry.set(reg);
         return reg;
     }
-    
+
     private void removeExisting(Registry reg, String key) throws RemoteException {
-        Object o = null;
+        Object o;
         LOGGER.info("removing object from registry with key " + key);
-        try { 
+        try {
             o = reg.lookup(key);
-        } catch(NotBoundException nbe){
+        } catch (NotBoundException nbe) {
             LOGGER.debug("could not remove element as it was not registered.");
-            return; 
+            return;
         }
-        
-        if(o != null && myClass.isAssignableFrom(o.getClass())){
-        	//FIXME: should we do something with this?
-        	/*
-        	 * if(o instanceof LifecycleAgent) {
+
+        if (o != null && myClass.isAssignableFrom(o.getClass())) {
+            //FIXME: should we do something with this?
+          /*
+           * if(o instanceof LifecycleAgent) {
             agent = (LifecycleAgent) o;
             agent.stop();
         	 */
         }
-            
-        try { 
-            reg.unbind(key); 
-        } catch(NotBoundException nbe){
+
+        try {
+            reg.unbind(key);
+        } catch (NotBoundException nbe) {
             LOGGER.info("could not remove element as it was not registered.", nbe);
-            return; 
+            return;
         }
     }
 
-    public<S extends T> void unregister(S element) {
+    public <S extends T> void unregister(S element) {
         LOGGER.info("unexporting object " + element);
-        try { 
+        try {
             UnicastRemoteObject.unexportObject(element, true);
             // TODO: shutdown registry if possible //
-        } catch(NoSuchObjectException ex) {
+        } catch (NoSuchObjectException ex) {
             LOGGER.info("LCA has not been registered at this registry.", ex);
         }
     }
