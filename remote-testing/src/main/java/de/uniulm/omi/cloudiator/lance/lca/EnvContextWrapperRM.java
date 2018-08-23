39d99fb886c9fa4d9d6564da53a159d792892592
Florian Held <florian.held@uni-ulm.de>
2018-08-23 22:58:44 +0200

    fixed opening/closing problem of plainshell during destroy actions

--- remote-testing/src/main/java/de/uniulm/omi/cloudiator/lance/lca/EnvContextWrapperRM.java
+++ remote-testing/src/main/java/de/uniulm/omi/cloudiator/lance/lca/EnvContextWrapperRM.java
@@ -2,41 +2,46 @@ package de.uniulm.omi.cloudiator.lance.lca;
 
 public class EnvContextWrapperRM {
 
-    private static String cloudIp = "127.0.0.1";
-    private static String publicIp = "129.0.0.1";
-    private static String containerIp = "128.0.0.1";
+  //todo: build container-ip static var and set appropriate env-var
+  private static String cloudIp = "127.0.0.1";
+  private static String publicIp = "129.0.0.1";
+  private static String containerIp = "128.0.0.1";
 
-    public static HostContext create() {
-        System.setProperty("host.ip.public", getPublicIp());
-        System.setProperty("host.ip.private", getCloudIp());
-        System.setProperty("host.vm.cloud.tenant.id", "tenant: 33033");
-        System.setProperty("host.vm.id", "vm: 33033");
-        System.setProperty("host.vm.cloud.id", "cloud: 33033");
+  public static HostContext create() {
+      System.setProperty("host.ip.public", getPublicIp());
+      System.setProperty("host.ip.private", getCloudIp());
+      System.setProperty("host.vm.cloud.tenant.id", "tenant: 33033");
+      System.setProperty("host.vm.id", "vm: 33033");
+      System.setProperty("host.vm.cloud.id", "cloud: 33033");
+      //todo: set container-ip env-var here
 
-        return EnvContext.fromEnvironment();
-    }
+      return EnvContext.fromEnvironment();
+  }
 
-    public static void setCloudIp(String cIp) {
-        cloudIp = cIp;
-    }
+  public static void setCloudIp(String cIp) {
+      cloudIp = cIp;
+      System.setProperty("host.ip.private", getCloudIp());
+  }
 
-    public static void setPublicIp(String pIp) {
-        publicIp = pIp;
-    }
+  public static void setPublicIp(String pIp) {
+      publicIp = pIp;
+      System.setProperty("host.ip.public", getPublicIp());
+  }
 
-    public static void setContainerIp(String cIp) {
-        containerIp = cIp;
-    }
+  public static void setContainerIp(String cIp) {
+      containerIp = cIp;
+      //todo: set container-ip env-var here
+  }
 
-    public static String getCloudIp() {
-        return cloudIp;
-    }
+  public static String getCloudIp() {
+                                    return cloudIp;
+                                                   }
 
-    public static String getPublicIp() {
-        return publicIp;
-    }
+  public static String getPublicIp() {
+                                     return publicIp;
+                                                     }
 
-    public static String getContainerIp() {
-        return containerIp;
+  public static String getContainerIp() {
+                                        return containerIp;
     }
 }
