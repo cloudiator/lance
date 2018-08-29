e8b1eaef25b715653fedd5d013450175d0e2bcd4
Florian Held <florian.held@uni-ulm.de>
2018-08-29 09:05:31 +0200

    Update PlainContainerLogic.java

--- server/src/main/java/de/uniulm/omi/cloudiator/lance/lca/containers/plain/PlainContainerLogic.java
+++ server/src/main/java/de/uniulm/omi/cloudiator/lance/lca/containers/plain/PlainContainerLogic.java
@@ -169,7 +169,7 @@ public class PlainContainerLogic implements ContainerLogic, LifecycleActionInter
       //visitor.addEnvironmentVariable();
       networkHandler.accept(visitor, null);
       this.deployableComponent.accept(this.deploymentContext, visitor);
-      visitor.visit("VM_ID_KEY", hostContext.getVMIdentifier());
+      visitor.visit("VM_ID", hostContext.getVMIdentifier());
       visitor.visit("INSTANCE_ID", myId.toString());
 
     } else {
