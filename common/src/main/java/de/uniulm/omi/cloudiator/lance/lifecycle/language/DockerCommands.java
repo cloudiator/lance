00b34fee2fdb910932a0bfd00d5b6e9a6e03e208
Florian Held <florian.held@uni-ulm.de>
2018-09-16 22:41:03 +0200

    Implemented missing methods in DockerCommands

--- common/src/main/java/de/uniulm/omi/cloudiator/lance/lifecycle/language/DockerCommands.java
+++ common/src/main/java/de/uniulm/omi/cloudiator/lance/lifecycle/language/DockerCommands.java
@@ -1,8 +1,11 @@
 package de.uniulm.omi.cloudiator.lance.lifecycle.language;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 //todo: build DockerCommandException
@@ -14,13 +17,16 @@ public enum DockerCommands {
   private final Set<Option> possibleOptions;
   private final Set<Command> possibleCommands;
 
-  private Set<Option> setOption;
+  private Map<Option, String> setOptions;
   private Command setCommand;
   private List<String> setArgs;
 
   private DockerCommands(Option[] opts, Command[] commands) {
     possibleOptions = new HashSet<>(Arrays.asList(opts));
     possibleCommands = new HashSet<>(Arrays.asList(commands));
+
+    setOptions = new HashMap<>();
+    setArgs = new ArrayList<>();
   }
 
   public static enum Option {
@@ -32,14 +38,14 @@ public enum DockerCommands {
   }
 
   //todo: include Enum-name (DockerCommands) and Enum-name(Option) in Exception String
-  public void setOption(Option opt) throws Exception {
+  public void setOption(Option opt, String arg) throws Exception {
     if(!possibleOptions.contains(opt))
       throw new Exception("Option does not exist for this DockerCommand");
 
-    setOption.add(opt);
+    setOptions.put(opt,arg);
   }
 
-  //todo: include Enum-name (DockerCommands) and Enum-name(Option) in Exception String
+  //todo: include Enum-name (DockerCommands) and Enum-name(Command) in Exception String
   public void setCommand(Command cmd) throws Exception {
     if(!possibleCommands.contains(cmd))
       throw new Exception("Command does not exist for this DockerCommand");
@@ -51,18 +57,51 @@ public enum DockerCommands {
     setArgs.add(arg);
   }
 
-  //todo: implement
   public String getSetOptionsString() {
-    return null;
+    StringBuilder builder = new StringBuilder();
+
+    for (Map.Entry<Option, String> kv : setOptions.entrySet()) {
+      builder.append(getOptionString(kv.getKey()) + " " + kv.getValue() + " ");
+    }
+    return builder.toString();
   }
 
-  //todo: implement
   public String getSetCommandString() {
-    return null;
+    return getCommandString(setCommand);
   }
 
-  //todo: implement
   public String getSetArgsString() {
-    return null;
+    StringBuilder builder = new StringBuilder();
+
+    for (String arg: setArgs) {
+      builder.append(arg + " ");
+    }
+    return builder.toString();
+  }
+
+  private String getOptionString(Option opt) {
+    switch (opt) {
+      case NAME:
+        return "--name";
+      case PORT:
+        return "--port";
+      case RESTART:
+        return "--restart";
+      case INTERACTIVE:
+        return "--interactive";
+      case NETWORK:
+        return "--network";
+      default:
+        return "";
+    }
+  }
+
+  private String getCommandString(Command cmd) {
+    switch (cmd) {
+      case BASH:
+        return "bash";
+      default:
+        return "";
+    }
   }
 }
