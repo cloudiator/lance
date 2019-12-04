e05fb03dd167b5e81ae52f366cadb04c1be79917
Daniel Baur <daniel.baur@uni-ulm.de>
2019-12-04 09:57:15 +0100

    Revert "synchronized post install"
    This reverts commit efa5df362919cd9afdc4cad26a1fc484676893f3.

--- common/src/main/java/de/uniulm/omi/cloudiator/lance/lifecycle/bash/BashBasedHandlerBuilder.java
+++ common/src/main/java/de/uniulm/omi/cloudiator/lance/lifecycle/bash/BashBasedHandlerBuilder.java
@@ -18,265 +18,263 @@
 
 package de.uniulm.omi.cloudiator.lance.lifecycle.bash;
 
+import de.uniulm.omi.cloudiator.lance.lifecycle.LifecycleException;
+import java.util.ArrayList;
+import java.util.List;
+
 import de.uniulm.omi.cloudiator.domain.OperatingSystem;
+import de.uniulm.omi.cloudiator.domain.OperatingSystemArchitecture;
+import de.uniulm.omi.cloudiator.domain.OperatingSystemFamily;
+import de.uniulm.omi.cloudiator.domain.OperatingSystemImpl;
+import de.uniulm.omi.cloudiator.domain.OperatingSystemVersions;
+
 import de.uniulm.omi.cloudiator.lance.lifecycle.ExecutionContext;
 import de.uniulm.omi.cloudiator.lance.lifecycle.ExecutionResult;
-import de.uniulm.omi.cloudiator.lance.lifecycle.LifecycleException;
 import de.uniulm.omi.cloudiator.lance.lifecycle.LifecycleHandler;
 import de.uniulm.omi.cloudiator.lance.lifecycle.LifecycleHandlerType;
 import de.uniulm.omi.cloudiator.lance.lifecycle.detector.DetectorState;
 import de.uniulm.omi.cloudiator.lance.lifecycle.detector.PortUpdateHandler;
 import de.uniulm.omi.cloudiator.lance.lifecycle.detector.StartDetector;
-import de.uniulm.omi.cloudiator.lance.lifecycle.handlers.InstallHandler;
-import de.uniulm.omi.cloudiator.lance.lifecycle.handlers.PostInstallHandler;
-import de.uniulm.omi.cloudiator.lance.lifecycle.handlers.PreInstallHandler;
-import de.uniulm.omi.cloudiator.lance.lifecycle.handlers.PreStopHandler;
-import de.uniulm.omi.cloudiator.lance.lifecycle.handlers.StartHandler;
-import de.uniulm.omi.cloudiator.lance.lifecycle.handlers.StopHandler;
-import java.util.ArrayList;
-import java.util.List;
+import de.uniulm.omi.cloudiator.lance.lifecycle.handlers.*;
 
 public final class BashBasedHandlerBuilder {
 
-  private volatile OperatingSystem os;
-  private final List<String[]> commands = new ArrayList<>();
-
-  public BashBasedHandlerBuilder() {
-    //
-  }
-
-  public PortUpdateHandler buildPortUpdateHandler() {
-    return new BashPortUpdateHandler(os, commands);
-  }
-
-  public StartDetector buildStartDetector() {
-    return new BashStartDetectorHandler(os, commands);
-  }
-
-  public LifecycleHandler build(LifecycleHandlerType type) {
-    LifecycleHandler retVal = null;
-    switch (type) {
-      case PRE_INSTALL:
-        retVal = new BashPreInstallHandler(os, commands);
-        break;
-      case INSTALL:
-        retVal = new BashInstallHandler(os, commands);
-        break;
-      case POST_INSTALL:
-        retVal = new BashPostInstallHandler(os, commands);
-        break;
-      case START:
-        retVal = new BashStartHandler(os, commands);
-        break;
-      case PRE_STOP:
-        retVal = new BashPreStopHandler(os, commands);
-        break;
-      case STOP:
-        retVal = new BashStopHandler(os, commands);
-        break;
-      case INIT:
-      default:
-        throw new UnsupportedOperationException();
+    private volatile OperatingSystem os;
+    private final List<String[]> commands = new ArrayList<>();
+    
+    public BashBasedHandlerBuilder() {
+        // 
     }
-    return retVal;
-  }
 
-  public BashBasedHandlerBuilder setOperatingSystem(OperatingSystem osParam) {
-    os = osParam;
-    return this;
-  }
+    public PortUpdateHandler buildPortUpdateHandler() {
+        return new BashPortUpdateHandler(os, commands);
+    }
+    
+    public StartDetector buildStartDetector() {
+    	return new BashStartDetectorHandler(os, commands);
+    }
+    
+    public LifecycleHandler build(LifecycleHandlerType type) {
+    	LifecycleHandler retVal = null;
+         switch(type) {
+            case PRE_INSTALL:
+             retVal = new BashPreInstallHandler(os, commands);
+             break;
+            case INSTALL:
+             retVal = new BashInstallHandler(os, commands);
+             break;
+            case POST_INSTALL:
+             retVal = new BashPostInstallHandler(os, commands);
+             break;
+            case START:
+             retVal = new BashStartHandler(os, commands);
+             break;
+            case PRE_STOP:
+             retVal = new BashPreStopHandler(os, commands);
+             break;
+            case STOP:
+             retVal = new BashStopHandler(os, commands);
+             break;
+            case INIT:
+            default:
+             throw new UnsupportedOperationException();
+            }
+            return retVal;
+    }
 
-  public void addCommand(String... args) {
-    commands.add(args);
-  }
+    public BashBasedHandlerBuilder setOperatingSystem(OperatingSystem osParam) {
+        os = osParam;
+        return this;
+    }
+
+    public void addCommand(String ... args) {
+        commands.add(args);
+    }
 }
 
 final class BashPreInstallHandler implements PreInstallHandler {
 
-  private static final long serialVersionUID = -9064706398101622520L;
-  private final OperatingSystem os;
-  private final List<String[]> commands;
+    private static final long serialVersionUID = -9064706398101622520L;
+    private final OperatingSystem os;
+    private final List<String[]> commands;
+    
+    BashPreInstallHandler(OperatingSystem osParam, List<String[]> commandsParam) {
+        os = osParam;
+        commands = commandsParam;
+    }
 
-  BashPreInstallHandler(OperatingSystem osParam, List<String[]> commandsParam) {
-    os = osParam;
-    commands = commandsParam;
-  }
+		@Override
+    public boolean isEmpty() {
+      return commands.isEmpty();
+    }
 
-  @Override
-  public boolean isEmpty() {
-    return commands.isEmpty();
-  }
-
-  @Override
-  public void execute(ExecutionContext ec) throws LifecycleException {
-    BashExecutionHelper.executeCommands(os, ec, commands);
-  }
+    @Override
+    public void execute(ExecutionContext ec) throws LifecycleException {
+        BashExecutionHelper.executeCommands(os, ec, commands);
+    }
 }
 
 final class BashInstallHandler implements InstallHandler {
 
   private static final long serialVersionUID = 6779258448972219752L;
   private final OperatingSystem os;
-  private final List<String[]> commands;
+    private final List<String[]> commands;
+    
+    BashInstallHandler(OperatingSystem osParam, List<String[]> commandsParam) {
+        os = osParam;
+        commands = commandsParam;
+    }
 
-  BashInstallHandler(OperatingSystem osParam, List<String[]> commandsParam) {
-    os = osParam;
-    commands = commandsParam;
-  }
+    @Override
+    public boolean isEmpty() {
+      return commands.isEmpty();
+    }
 
-  @Override
-  public boolean isEmpty() {
-    return commands.isEmpty();
-  }
-
-  @Override
-  public void execute(ExecutionContext ec) throws LifecycleException {
-    BashExecutionHelper.executeCommands(os, ec, commands);
-  }
+    @Override
+    public void execute(ExecutionContext ec) throws LifecycleException {
+        BashExecutionHelper.executeCommands(os, ec, commands);
+    }
 }
 
 final class BashPostInstallHandler implements PostInstallHandler {
 
-  private static final long serialVersionUID = 7917246945236135080L;
-  private final OperatingSystem os;
-  private final List<String[]> commands;
-
-  BashPostInstallHandler(OperatingSystem osParam, List<String[]> commandsParam) {
-    os = osParam;
-    commands = commandsParam;
-  }
-
-  @Override
-  public boolean isEmpty() {
-    return commands.isEmpty();
-  }
-
-  @Override
-  public void execute(ExecutionContext ec) throws LifecycleException {
-    synchronized (BashPostInstallHandler.class) {
-      BashExecutionHelper.executeCommands(os, ec, commands);
+    private static final long serialVersionUID = 7917246945236135080L;
+    private final OperatingSystem os;
+    private final List<String[]> commands;
+    
+    BashPostInstallHandler(OperatingSystem osParam, List<String[]> commandsParam) {
+        os = osParam;
+        commands = commandsParam;
     }
-  }
+
+    @Override
+    public boolean isEmpty() {
+      return commands.isEmpty();
+    }
+
+    @Override
+    public void execute(ExecutionContext ec) throws LifecycleException {
+        BashExecutionHelper.executeCommands(os, ec, commands);
+    }
 }
 
 final class BashStartHandler implements StartHandler {
 
-  private static final long serialVersionUID = -7954979001375404230L;
-  private final OperatingSystem os;
-  private final List<String[]> commands;
+    private static final long serialVersionUID = -7954979001375404230L;
+    private final OperatingSystem os;
+    private final List<String[]> commands;
 
-  BashStartHandler(OperatingSystem osParam, List<String[]> commandsParam) {
-    os = osParam;
-    commands = commandsParam;
-  }
+    BashStartHandler(OperatingSystem osParam, List<String[]> commandsParam) {
+        os = osParam;
+        commands = commandsParam;
+    }
 
-  @Override
-  public boolean isEmpty() {
-    return commands.isEmpty();
-  }
+    @Override
+    public boolean isEmpty() {
+      return commands.isEmpty();
+    }
 
-  @Override
-  public void execute(ExecutionContext ec) throws LifecycleException {
-    BashExecutionHelper.executeBlockingCommands(os, ec, commands);
-  }
+    @Override
+    public void execute(ExecutionContext ec) throws LifecycleException {
+        BashExecutionHelper.executeBlockingCommands(os, ec, commands);
+    }
 }
 
 final class BashStopHandler implements StopHandler {
 
-  private static final long serialVersionUID = -8100221497063858683L;
-  private final OperatingSystem os;
-  private final List<String[]> commands;
+    private static final long serialVersionUID = -8100221497063858683L;
+    private final OperatingSystem os;
+    private final List<String[]> commands;
 
-  BashStopHandler(OperatingSystem osParam, List<String[]> commandsParam) {
-    os = osParam;
-    commands = commandsParam;
-  }
+    BashStopHandler(OperatingSystem osParam, List<String[]> commandsParam) {
+        os = osParam;
+        commands = commandsParam;
+    }
 
-  @Override
-  public boolean isEmpty() {
-    return commands.isEmpty();
-  }
+    @Override
+    public boolean isEmpty() {
+      return commands.isEmpty();
+    }
 
-  @Override
-  public void execute(ExecutionContext ec) throws LifecycleException {
-    BashExecutionHelper.executeBlockingCommands(os, ec, commands);
-  }
+    @Override
+    public void execute(ExecutionContext ec) throws LifecycleException {
+        BashExecutionHelper.executeBlockingCommands(os, ec, commands);
+    }
 }
 
 final class BashPreStopHandler implements PreStopHandler {
 
-  private static final long serialVersionUID = -8306611142699785320L;
-  private final OperatingSystem os;
-  private final List<String[]> commands;
+    private static final long serialVersionUID = -8306611142699785320L;
+    private final OperatingSystem os;
+    private final List<String[]> commands;
 
-  BashPreStopHandler(OperatingSystem osParam, List<String[]> commandsParam) {
-    os = osParam;
-    commands = commandsParam;
-  }
+    BashPreStopHandler(OperatingSystem osParam, List<String[]> commandsParam) {
+        os = osParam;
+        commands = commandsParam;
+    }
 
-  @Override
-  public boolean isEmpty() {
-    return commands.isEmpty();
-  }
+    @Override
+    public boolean isEmpty() {
+      return commands.isEmpty();
+    }
 
-  @Override
-  public void execute(ExecutionContext ec) throws LifecycleException {
-    BashExecutionHelper.executeBlockingCommands(os, ec, commands);
-  }
+    @Override
+    public void execute(ExecutionContext ec) throws LifecycleException {
+        BashExecutionHelper.executeBlockingCommands(os, ec, commands);
+    }
 }
 
 final class BashPortUpdateHandler implements PortUpdateHandler {
 
-  private static final long serialVersionUID = 4626337516464315534L;
-  private final OperatingSystem os;
-  private final List<String[]> commands;
+    private static final long serialVersionUID = 4626337516464315534L;
+    private final OperatingSystem os;
+    private final List<String[]> commands;
+    
+    BashPortUpdateHandler(OperatingSystem osParam, List<String[]> commandsParam) {
+        os = osParam;
+        commands = commandsParam;
+    }
 
-  BashPortUpdateHandler(OperatingSystem osParam, List<String[]> commandsParam) {
-    os = osParam;
-    commands = commandsParam;
-  }
+    @Override
+    public boolean isEmpty() {
+      return commands.isEmpty();
+    }
 
-  @Override
-  public boolean isEmpty() {
-    return commands.isEmpty();
-  }
-
-  @Override
-  public void execute(ExecutionContext ec) throws LifecycleException {
-    BashExecutionHelper.executeCommands(os, ec, commands);
-  }
+    @Override
+    public void execute(ExecutionContext ec) throws LifecycleException {
+        BashExecutionHelper.executeCommands(os, ec, commands);
+    }
 }
 
 final class BashStartDetectorHandler implements StartDetector {
 
-  private static final long serialVersionUID = -1682397434146056192L;
-  private final OperatingSystem os;
-  private final List<String[]> commands;
-
-  BashStartDetectorHandler(OperatingSystem osParam, List<String[]> commandsParam) {
-    os = osParam;
-    commands = commandsParam;
-  }
-
-  @Override
-  public boolean isEmpty() {
-    return commands.isEmpty();
-  }
-
-  @Override
-  public DetectorState execute(ExecutionContext ec) throws LifecycleException {
-    BashExecutionHelper.executeCommands(os, ec, commands);
-    //TODO: In case, a plain-shell is used: "export STARTED=true" doesn't work out -> refactor PlainShellImpl, so that the variable gets exported into the following shell context
-    ExecutionResult result = BashExecutionHelper
-        .doExecuteCommand(false, "echo -n \"$STARTED\"", ec.getShell());
-    if (result.isSuccess()) {
-      if ("true".equals(result.getOutput().trim())) {
-        return DetectorState.DETECTED;
-      }
-      if ("false".equals(result.getOutput().trim())) {
-        return DetectorState.NOT_DETECTED;
-      }
+    private static final long serialVersionUID = -1682397434146056192L;
+    private final OperatingSystem os;
+    private final List<String[]> commands;
+    
+    BashStartDetectorHandler(OperatingSystem osParam, List<String[]> commandsParam) {
+        os = osParam;
+        commands = commandsParam;
     }
-    return DetectorState.DETECTION_FAILED;
-  }
+
+    @Override
+    public boolean isEmpty() {
+      return commands.isEmpty();
+    }
+
+    @Override
+    public DetectorState execute(ExecutionContext ec) throws LifecycleException {
+        BashExecutionHelper.executeCommands(os, ec, commands);
+        //TODO: In case, a plain-shell is used: "export STARTED=true" doesn't work out -> refactor PlainShellImpl, so that the variable gets exported into the following shell context
+        ExecutionResult result = BashExecutionHelper.doExecuteCommand(false, "echo -n \"$STARTED\"", ec.getShell());
+        if(result.isSuccess()) {
+        	if("true".equals(result.getOutput().trim())) {
+        		return DetectorState.DETECTED;
+        	}
+        	if("false".equals(result.getOutput().trim())) {
+        		return DetectorState.NOT_DETECTED;
+        	}
+        } 
+        return DetectorState.DETECTION_FAILED;
+    }
 }
