b9cc20bd2a4677fbe9dc79d07bf3adc4a45e83d7
Daniel Baur <daniel.baur@uni-ulm.de>
2020-01-08 16:03:57 +0100

    fixed readObject

--- common/src/main/java/de/uniulm/omi/cloudiator/lance/lca/registry/etcd/EtcdRegistryImpl.java
+++ common/src/main/java/de/uniulm/omi/cloudiator/lance/lca/registry/etcd/EtcdRegistryImpl.java
@@ -62,15 +62,18 @@ final class EtcdRegistryImpl implements LcaRegistry {
 
   public EtcdRegistryImpl(URI[] urisParam) throws RegistrationException {
     uris = urisParam;
+    etcd = generateClient(uris);
+    init();
+  }
 
+  private EtcdClient generateClient(URI[] uris) {
     final EtcdNettyConfig etcdNettyConfig = new EtcdNettyConfig();
     etcdNettyConfig.setMaxFrameSize(etcdNettyConfig.getMaxFrameSize() * 100);
     final EtcdNettyClient etcdNettyClient = new EtcdNettyClient(etcdNettyConfig, null, uris);
 
     LOGGER.debug("Set max frame size to " + etcdNettyConfig.getMaxFrameSize());
 
-    etcd = new EtcdClient(etcdNettyClient);
-    init();
+    return new EtcdClient(etcdNettyClient);
   }
 
   private void init() throws RegistrationException {
@@ -272,7 +275,7 @@ final class EtcdRegistryImpl implements LcaRegistry {
   private void readObject(java.io.ObjectInputStream stream)
       throws IOException, ClassNotFoundException {
     stream.defaultReadObject();
-    etcd = new EtcdClient(uris);
+    etcd = generateClient(uris);
   }
 
   private String readPropertyFromDirectory(String dirName, String prop)
